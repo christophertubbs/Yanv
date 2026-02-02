7d274c1394c1da2c614ebb80aeff22f71c043e17
christopher.tubbs <christopher.tubbs@noaa.gov>
2026-02-02 12:18:43 -0600

    Updated __main__ to work better from the command line

--- yanv/__main__.py
+++ yanv/__main__.py
@@ -3,6 +3,7 @@ Runs YANV from the module level
 """
 import sys
 import logging
+import logging.handlers
 import typing
 
 from yanv.launch_parameters import ApplicationArguments
@@ -13,7 +14,14 @@ if __name__.endswith("__main__"):
         level=logging.INFO,
         format="[%(asctime)s] %(levelname)s %(name)s %(lineno)d: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S%z",
-        force=True
+        force=True,
+        handlers=[
+            logging.StreamHandler(),
+            logging.handlers.RotatingFileHandler(
+                filename="yanv.log",
+                maxBytes=5 * 1000 * 1000,
+            )
+        ]
     )
     aiohttp_logger: logging.Logger = logging.getLogger("aiohttp")
     aiohttp_logger.setLevel(logging.WARNING)
@@ -23,6 +31,7 @@ if __name__.endswith("__main__"):
 
 def main() -> typing.NoReturn:
     try:
+        logging.info(f"Launching yanv from a module level...")
         exit_code: int = serve(ApplicationArguments(*sys.argv[1:]))
     except KeyboardInterrupt:
         logging.critical(f"Uncaught keyboard interrupt encountered. Exitting...")
@@ -32,3 +41,7 @@ def main() -> typing.NoReturn:
         exit_code: int = 1
 
     sys.exit(exit_code)
+
+
+if __name__ == "__main__":
+    main()
